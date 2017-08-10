9dfcad28077841e5bc81379cb3b8f7bb42bca1fa
micaksica <micaksica@users.noreply.github.com>
2017-08-09 21:14:32 -0700

    Move bcrypt constants to header per @daffl

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,10 +1,11 @@
 import bcrypt from 'bcryptjs';
 
+const BCRYPT_WORK_FACTOR_BASE = 12;
+const BCRYPT_DATE_BASE = 1483228800000;
+const BCRYPT_WORK_INCREASE_INTERVAL = 47300000000;
+
 export default function hasher (password) {
   return new Promise((resolve, reject) => {
-    const BCRYPT_WORK_FACTOR_BASE = 12;
-    const BCRYPT_DATE_BASE = 1483228800000;
-    const BCRYPT_WORK_INCREASE_INTERVAL = 47300000000;
     let BCRYPT_CURRENT_DATE = new Date().getTime();
     let BCRYPT_WORK_INCREASE = Math.max(0, Math.floor((BCRYPT_CURRENT_DATE - BCRYPT_DATE_BASE) / BCRYPT_WORK_INCREASE_INTERVAL));
     let BCRYPT_WORK_FACTOR = Math.min(19, BCRYPT_WORK_FACTOR_BASE + BCRYPT_WORK_INCREASE);
